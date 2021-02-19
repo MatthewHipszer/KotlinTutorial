d8123e0374375f864e0a72f751e18d0f6dfa453b
MatthewHipszer <73508557+MatthewHipszer@users.noreply.github.com>
2021-02-18 22:24:47 -0500

    Messing with JLabels and GroupLayout.

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,8 @@
+import java.awt.Color
+import java.awt.Dimension
 import java.awt.EventQueue
 import javax.swing.*
+import javax.swing.SwingConstants.LEADING
 import kotlin.system.exitProcess
 
 class Main(title: String) : JFrame() {
@@ -10,29 +13,76 @@ class Main(title: String) : JFrame() {
 
     private fun createUI(title: String) {
 
-        setTitle(title)
+        val colorArray = arrayOf<Color>(Color.black, Color.blue, Color.cyan,
+            Color.darkGray, Color.gray, Color.green, Color.lightGray, Color.magenta,
+            Color.orange, Color.pink, Color.red, Color.white, Color.yellow)
 
-        val closeButton = JButton("Close")
+        val labels = colorArray.map {
+            JLabel("", null, LEADING).apply {
+                minimumSize = Dimension(90, 40)
+                background = it
+                isOpaque = true
+            }
+        }
+
+        val closeButton = JButton("Close").apply {
+            minimumSize = Dimension(90, 40)
+            isOpaque = true
+        }
         closeButton.addActionListener { exitProcess(0) }
-        createLayout(closeButton)
+
+        createLayout(labels, closeButton)
+
+        setTitle(title)
 
         defaultCloseOperation = EXIT_ON_CLOSE
         setSize(800, 500)
         setLocationRelativeTo(null)
     }
 
-    private fun createLayout(vararg arg: JComponent) {
+    private fun createLayout(labels: List<JLabel>, vararg arg: JComponent) {
 
         val gl = GroupLayout(contentPane)
         contentPane.layout = gl
 
         gl.autoCreateContainerGaps = true
+        gl.autoCreateGaps = true
 
-        gl.setHorizontalGroup(gl.createSequentialGroup()
+        gl.setHorizontalGroup(gl.createParallelGroup()
+            .addGroup(gl.createSequentialGroup()
+                .addComponent(labels[0])
+                .addComponent(labels[1])
+                .addComponent(labels[2])
+                .addComponent(labels[3]))
+            .addGroup(gl.createSequentialGroup()
+                .addComponent(labels[4])
+                .addComponent(labels[5])
+                .addComponent(labels[6])
+                .addComponent(labels[7])
+                .addComponent(labels[8])
+                .addComponent(labels[9])
+                .addComponent(labels[10])
+                .addComponent(labels[11]))
+            .addComponent(labels[12])
             .addComponent(arg[0])
         )
 
         gl.setVerticalGroup(gl.createSequentialGroup()
+            .addGroup(gl.createParallelGroup()
+                .addComponent(labels[0])
+                .addComponent(labels[1])
+                .addComponent(labels[2])
+                .addComponent(labels[3]))
+            .addGroup(gl.createParallelGroup()
+                .addComponent(labels[4])
+                .addComponent(labels[5])
+                .addComponent(labels[6])
+                .addComponent(labels[7])
+                .addComponent(labels[8])
+                .addComponent(labels[9])
+                .addComponent(labels[10])
+                .addComponent(labels[11]))
+            .addComponent(labels[12])
             .addComponent(arg[0])
         )
 
